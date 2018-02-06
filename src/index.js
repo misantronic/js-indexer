ceea521bed5bc86e146b3c434f1b13e5d6230405
David Schkalee <david.schkalee@magicline.de>
2018-02-06 16:39:22 +0100

    feat: allow multiple extensions

--- src/index.js
+++ src/index.js
@@ -11,16 +11,21 @@ const files = cli.input;
 files.forEach(file => {
     const dir = dirname(file);
     const ext = extname(file);
+    const exts = [ext, '.js', '.jsx', '.ts', '.tsx'];
 
     const content =
-        sync(`${dir}/**/!(*[._]test)${ext}`)
+        sync(
+            `${dir}/**/!(*[._]test).{${exts
+                .map(ext => ext.substr(1))
+                .join(',')}}`
+        )
             .filter(file => file !== `${dir}/index${ext}`)
             .map(
                 line =>
                     "export * from '" +
                     line
                         .replace(`${dir}/`, './')
-                        .replace(ext, '')
+                        .replace(new RegExp(exts.join('|'), 'g'), '')
                         .replace('/index', '') +
                     "';"
             )
